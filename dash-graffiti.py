8ef414be4e9e15e085a3342825244129b9d64d99
rockbit <rockbit6@gmail.com>
2017-07-09 12:29:51 +0200

    refactor as library graffity.py

--- dash-graffiti.py
+++ dash-graffiti.py
@@ -1,38 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from lib.b58 import *
-from lib.hashs import *
+from lib.graffiti import *
 
-long = int
-_bchr = lambda x: bytes([x])
+string = "Thanks Evan Duffield"
 
-data = "Thanks Evan Duffield"
-
-length = len(data)
-print("Data lenght: " + str(length))
-if length > 20:
-   print("ERROR: Input string is too long")
-   print("Insert 20 characters or less")
-   exit()
-
-for x in range(length, 20):
-   data = data + "\x00"
-
-data_as_byte = str.encode(data)
-
-addr_prefix = 76  # 4c
-addr_prefix_as_byte = _bchr(addr_prefix)
-
-vs = addr_prefix_as_byte + data_as_byte
-# print(vs)
-# vshex = binascii.hexlify(data_as_byte)
-# print(vshex)
-
-check = double_sha256(vs)[0:4]
-
-encpay = vs + check
-
-base58_encpay = b58encode(encpay)
-print('Write graffiti "' + data + '" to blockchain by sending some duffs to:\n')
-print(base58_encpay)
+print('Write graffiti "' + string + '" to blockchain by sending some duffs to:\n')
+print(generate_graffiti_address(string))
---
+++ lib/graffiti.py
@@ -0,0 +1,37 @@
+# graffiti.py
+
+from lib.hashs import *
+from lib.b58 import *
+
+long = int
+_bchr = lambda x: bytes([x])
+_bord = lambda x: x
+
+def generate_graffiti_address(string): # from string
+   data = string
+   length = len(data)
+   print("Data lenght: " + str(length))
+   if length > 20:
+      print("ERROR: Input string is too long")
+      print("Insert 20 characters or less")
+      return
+   
+   for x in range(length, 20):
+      data = data + "\x00"
+   
+   data_as_byte = str.encode(data)
+   
+   addr_prefix = 76  # 4c
+   addr_prefix_as_byte = _bchr(addr_prefix)
+   
+   vs = addr_prefix_as_byte + data_as_byte
+   # print(vs)
+   # vshex = binascii.hexlify(data_as_byte)
+   # print(vshex)
+   
+   check = double_sha256(vs)[0:4]
+   
+   encpay = vs + check
+   
+   base58_encpay = b58encode(encpay)
+   return base58_encpay
