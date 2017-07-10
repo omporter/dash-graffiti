cbb20350f84fc8471e0758dcbcc1ddd1d0aa5bb1
rockbit <rockbit6@gmail.com>
2017-07-10 13:21:56 +0200

    add generate_graffii call to webservice

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from lib.graffiti import generate_graffiti_address
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Yo, its working!'
+	return generate_graffiti_address("Heroku Test")
 
 if __name__ == "__main__":
 	app.run()
