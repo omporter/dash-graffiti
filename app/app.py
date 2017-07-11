77ff54fb9806dffd6f28ecf422902cf1867612f3
moocowmoo <moocowmoo@masternode.me>
2017-07-11 04:52:57 +0000

    style - retab, pep8, cleanup

--- app/app.py
+++ app/app.py
@@ -7,27 +7,27 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27fdlk43jvn4d441f2b6176a'
 
+
 class ReusableForm(Form):
-    string = TextField('Graffiti (max 20 chars):', validators=[validators.required()])
+    string = TextField(
+        'Graffiti (max 20 chars):', validators=[validators.required()])
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	form = ReusableForm(request.form)
-	if request.method == 'POST':
-		string = request.form['string']
-		print(string)
-		
-		if form.validate():
-			# Save the comment here.
-			flash('Write graffiti "' + string + '" to blockchain by sending some duffs to:')
-			flash('')
-			flash(generate_graffiti_address(string))
-		else:
-			flash('All the form fields are required. ')
+    form = ReusableForm(request.form)
+    if request.method == 'POST':
+        string = request.form['string']
+        if form.validate():
+            # Save the comment here.
+            flash('Write graffiti "' + string +
+                  '" to blockchain by sending some duffs to:')
+            flash('')
+            flash(generate_graffiti_address(string))
+        else:
+            flash('All the form fields are required. ')
 
-	return render_template('index.html', form=form)
-	# return generate_graffiti_address("Heroku Test")
+    return render_template('index.html', form=form)
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
