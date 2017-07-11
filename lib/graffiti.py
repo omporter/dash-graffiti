6155be36b9b0df23a6e6d19b0b3b25bed23a6bd7
rockbit <rockbit6@gmail.com>
2017-07-11 15:38:07 +0200

    update

--- lib/graffiti.py
+++ lib/graffiti.py
@@ -15,7 +15,8 @@ def generate_graffiti_address(string):  # from string
    if length > 20:
       print("ERROR: Input string is too long")
       print("Insert 20 characters or less")
-      return "ERROR: Input string is too long"
+      errmsg = "ERROR: Input string is too long - " + length + "chars"
+      return errmsg
    
    for x in range(length, 20):
       data = data + "\x00"
